dab3d4f8ff060906792e63396d51cb7c1ff70ad4
Moo-hi <viljami.j.m@gmail.com>
2022-02-17 21:26:35 +0200

    Implement page routing using react-router-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Jasenet from "Jasenet";
+import Matkakohteet from "./Matkakohteet";
+import OmatMatkat from "OmatMatkat";
+import OmatTiedot from "./OmatTiedot";
+import PorukanMatkat from "./PorukanMatkat";
+import Rekisterointi from "./Rekisterointi";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route exact path="/jasenet" element={<Jasenet />} />
+        <Route exact path="/matkakohteet" element={<Matkakohteet />} />
+        <Route exact path="/omat_matkat" element={<OmatMatkat />} />
+        <Route exact path="/omat_tiedot" element={<OmatTiedot />} />
+        <Route exact path="/porukan_matkat" element={<PorukanMatkat />} />
+        <Route exact path="/rekisterointi" element={<Rekisterointi />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
