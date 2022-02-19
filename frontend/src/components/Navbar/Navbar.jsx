a8077d4e5bd02208d6faca90f25e514c9a711bba
Moo-hi <viljami.j.m@gmail.com>
2022-02-19 13:44:49 +0200

    Create StyledLink component
    Replaces the old hacky way of moving the <Link> text to right place.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import { AppBar, Box, Link, Toolbar } from "@mui/material";
+import { AppBar, Box, Toolbar } from "@mui/material";
 import NavbarButtons from "./NavbarButtons";
 import PropTypes from "prop-types";
+import StyledLink from "./styled/StyledLink";
 
 Navbar.defaultProps = {
   loggedInName: "",
@@ -16,19 +17,7 @@ export default function Navbar({ loggedInName }) {
       <Box>
         <AppBar elevation={0} color="transparent" position="static">
           <Toolbar sx={{ mr: 5 }}>
-            <Link
-              href="/"
-              variant="h6"
-              sx={{
-                flexGrow: 1,
-                fontWeight: "medium",
-                textDecoration: "none",
-                color: "black",
-              }}
-            >
-              ⠀⠀⠀ Kuopion Kulkijat
-              {/* Hacky fix using invisible characters to get the text in right place - couldn't apply margin, probably something to do with flex */}
-            </Link>
+            <StyledLink href="/">Kuopion Kulkijat</StyledLink>
             {<NavbarButtons loggedInName={loggedInName} />}
           </Toolbar>
         </AppBar>
