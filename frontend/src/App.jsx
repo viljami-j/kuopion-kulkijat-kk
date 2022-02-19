7e616434d652203eeaa9fd7ec5fa1e8645f6ee7b
Moo-hi <viljami.j.m@gmail.com>
2022-02-19 16:19:38 +0200

    Create rest of Hero-component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "theme";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Navbar />
+        <Hero
+          imageSrc="https://cdn.wallpapersafari.com/94/43/bf8BcM.jpg"
+          header="Tervetuloa kulkemaan kanssamme!"
+          caption="Tutustu maailmaan yhdessä tuhansien käyttäjiemme kanssa. Älä kulje
+          enää yksin. Ota Kuopion Kulkijat mukaasi."
+        />
       </ThemeProvider>
     </BrowserRouter>
   );
--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,5 +1,49 @@
-import * as React from "react";
+import { Box, Grid, Typography } from "@mui/material";
+import PropTypes from "prop-types";
 
-function Hero() {
-  return <div></div>;
+export default function Hero({ imageSrc, header, caption }) {
+  return (
+    <Grid item xs={12} align="center">
+      <Box
+        sx={{
+          marginTop: "25px",
+          backgroundImage: `url(${imageSrc})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "800px",
+          width: "92%",
+          borderRadius: "10px",
+        }}
+      >
+        <Typography
+          variant="h2"
+          style={{ color: "white", position: "relative", top: "50px" }}
+        >
+          {header}
+        </Typography>
+        <Typography
+          style={{
+            color: "white",
+            position: "relative",
+            top: "80px",
+            width: "600px",
+          }}
+        >
+          {caption}
+        </Typography>
+      </Box>
+    </Grid>
+  );
 }
+
+Hero.defaultProps = {
+  imageSrc: "",
+  header: "",
+  caption: "",
+};
+
+Hero.propTypes = {
+  imageSrc: PropTypes.string,
+  header: PropTypes.string,
+  caption: PropTypes.string,
+};
