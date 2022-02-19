b1ef46653d9e00e27ca353e63f49c298901b22a9
ArttuOll <arttuoll.tech@tutanota.com>
2022-02-19 14:04:51 +0200

    Create Footer component

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+function Footer() {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between", my: 6 }}>
+      <Box>
+        <Typography fontWeight="medium">Kuopion Kulkijat</Typography>
+        <Typography>Älä kulje yksin</Typography>
+      </Box>
+      <Box>
+        <Typography fontWeight="medium">Tekijät</Typography>
+        <Typography>Eetu Ollanketo</Typography>
+        <Typography>Viljami Mäkelä</Typography>
+        <Typography>Artturi Toivainen</Typography>
+        <Typography>Arttu Olli</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default Footer;
--- frontend/src/scenes/front-page/FrontPage.jsx
+++ frontend/src/scenes/front-page/FrontPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "@mui/material";
 import SuggestedDestinations from "./SuggestedDestinations";
+import Footer from "../../components/Footer/Footer";
 
 const FrontPage = () => {
   return (
     <Container>
       <SuggestedDestinations />
+      <Footer />
     </Container>
   );
 };
