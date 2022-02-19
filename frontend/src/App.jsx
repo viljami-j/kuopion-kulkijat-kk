273ce1026f5db6f5c9b1613efa52b1949118641d
ArttuOll <arttuoll.tech@tutanota.com>
2022-02-19 12:49:58 +0200

    Fix nav bar not being displayed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "theme";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Routes></Routes>
+        <Navbar />
       </ThemeProvider>
     </BrowserRouter>
   );
