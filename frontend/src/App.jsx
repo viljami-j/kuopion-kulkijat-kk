5874d54a5b40c04529111b0efa5f279890d70424
ArttuOll <arttuoll@tutanota.com>
2022-02-24 20:13:52 +0200

    Make navbar and footer common to whole app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "theme";
 import FrontPage from "scenes/front-page/FrontPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DestinationSearch from "scenes/destination-search/DestinationSearch";
+import Navbar from "./components/Navbar/Navbar";
+import React from "react";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Routes>
-          <Route path="/" element={<FrontPage />}>
-            <Route path="destinations" element={<DestinationSearch />} />
-          </Route>
-        </Routes>
+        <Container>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<FrontPage />}>
+              <Route path="destinations" element={<DestinationSearch />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </Container>
       </ThemeProvider>
     </BrowserRouter>
   );
--- frontend/src/scenes/front-page/FrontPage.jsx
+++ frontend/src/scenes/front-page/FrontPage.jsx
@@ -1,27 +1,20 @@
 import React from "react";
-import { Container } from "@mui/material";
 import SuggestedDestinations from "./SuggestedDestinations";
-import Footer from "../../components/Footer/Footer";
-import Navbar from "components/Navbar/Navbar";
 import Hero from "components/Hero/Hero";
 import HeroImage from "images/hero.jpg";
 
 const FrontPage = () => {
   return (
-    <Container>
-      <Navbar />
-      <main>
-        <Hero
-          imageSrc={HeroImage}
-          header={"Tervetuloa kulkemaan kanssamme"}
-          caption={
-            "Tutustu maailmaan yhdessä tuhansien käyttäjiemme kanssa. Älä kulje enää yksin. Ota Kuopion Kulkijat mukaasi."
-          }
-        />
-        <SuggestedDestinations />
-      </main>
-      <Footer />
-    </Container>
+    <main>
+      <Hero
+        imageSrc={HeroImage}
+        header={"Tervetuloa kulkemaan kanssamme"}
+        caption={
+          "Tutustu maailmaan yhdessä tuhansien käyttäjiemme kanssa. Älä kulje enää yksin. Ota Kuopion Kulkijat mukaasi."
+        }
+      />
+      <SuggestedDestinations />
+    </main>
   );
 };
 
