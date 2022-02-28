47a2d54da0898365557c1c491a7790fa3f5431fe
ArttuOll <arttuoll@tutanota.com>
2022-02-28 20:17:04 +0200

    Add destination card grid to destination search

--- frontend/src/scenes/destination-search/DestinationSearch.jsx
+++ frontend/src/scenes/destination-search/DestinationSearch.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { Box, TextField, Typography } from "@mui/material";
+import DestinationCardGrid from "../../components/DestinationCardGrid";
+import testDestinations from "test-data/testDestinations.json";
 
 function DestinationSearch() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        alignItems: "center",
-        mt: 8,
-        width: "100%",
-      }}
-    >
-      <Typography variant={"h1"} sx={{ mb: 7 }}>
-        Matkakohdehaku
-      </Typography>
-      <TextField
-        id={"destination-search-field"}
-        label={"Etsi matkakohdetta"}
-        variant={"outlined"}
+    <Box>
+      <Box
         sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          alignItems: "center",
+          mt: 8,
           width: "100%",
         }}
-      />
+      >
+        <Typography variant={"h1"} sx={{ mb: 7 }}>
+          Matkakohdehaku
+        </Typography>
+        <TextField
+          id={"destination-search-field"}
+          label={"Etsi matkakohdetta"}
+          variant={"outlined"}
+          sx={{
+            width: "100%",
+          }}
+        />
+      </Box>
+      <DestinationCardGrid destinations={testDestinations} />
     </Box>
   );
 }
