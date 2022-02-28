02bc576b8c002ed4c19c6e8a9e7b2157073abcd9
ArttuOll <arttuoll@tutanota.com>
2022-02-28 19:24:25 +0200

    Configure routing to /destinations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Container, CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "theme";
 import FrontPage from "scenes/front-page/FrontPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import DestinationSearch from "scenes/destination-search/DestinationSearch";
 import Navbar from "./components/Navbar/Navbar";
 import React from "react";
@@ -9,20 +9,17 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Container>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<FrontPage />}>
-              <Route path="destinations" element={<DestinationSearch />} />
-            </Route>
-          </Routes>
-          <Footer />
-        </Container>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<FrontPage />} />
+          <Route path="destinations" element={<DestinationSearch />} />
+        </Routes>
+        <Footer />
+      </Container>
+    </ThemeProvider>
   );
 }
 
