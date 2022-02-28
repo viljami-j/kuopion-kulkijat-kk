d16cf435c9c0d3b6f2a30b71699766a759e4bf34
ArttuOll <arttuoll@tutanota.com>
2022-02-28 20:10:20 +0200

    Add destination search view with search bar

--- frontend/src/scenes/destination-search/DestinationSearch.jsx
+++ frontend/src/scenes/destination-search/DestinationSearch.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Box, TextField, Typography } from "@mui/material";
 
-const DestinationSearch = () => {
-  return <div></div>;
-};
+function DestinationSearch() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        alignItems: "center",
+        mt: 8,
+        width: "100%",
+      }}
+    >
+      <Typography variant={"h1"} sx={{ mb: 7 }}>
+        Matkakohdehaku
+      </Typography>
+      <TextField
+        id={"destination-search-field"}
+        label={"Etsi matkakohdetta"}
+        variant={"outlined"}
+        sx={{
+          width: "100%",
+        }}
+      />
+    </Box>
+  );
+}
 
 export default DestinationSearch;
