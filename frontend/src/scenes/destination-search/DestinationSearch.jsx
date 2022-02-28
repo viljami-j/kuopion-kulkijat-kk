261aad6b3290d05fe2dd9649b622ebf4b1e60327
ArttuOll <arttuoll@tutanota.com>
2022-02-28 20:32:43 +0200

    Make destination search field controlled component

--- frontend/src/scenes/destination-search/DestinationSearch.jsx
+++ frontend/src/scenes/destination-search/DestinationSearch.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Box, TextField, Typography } from "@mui/material";
-import DestinationCardGrid from "../../components/DestinationCardGrid";
+import DestinationCardGrid from "components/DestinationCardGrid";
 import testDestinations from "test-data/testDestinations.json";
 
 function DestinationSearch() {
   const [destinations, setDestinations] = useState();
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(function fetchSuggestedDestinations() {
     // TODO/30: fetch destinations from the destinations context variable once the endpoint is implemented
@@ -12,6 +13,10 @@ function DestinationSearch() {
       setDestinations(testDestinations);
     }, 1000);
   }, []);
+
+  function onTextFieldTyped(event) {
+    setSearchQuery(event.target.value);
+  }
 
   return (
     <main>
@@ -32,6 +37,8 @@ function DestinationSearch() {
           id={"destination-search-field"}
           label={"Etsi matkakohdetta"}
           variant={"outlined"}
+          value={searchQuery}
+          onChange={onTextFieldTyped}
           sx={{
             width: "100%",
           }}
