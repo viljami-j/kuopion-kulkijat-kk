21f895400ea3507ae1f1acff0e768f7e7bec75d5
Moo-hi <viljami.j.m@gmail.com>
2022-03-05 18:18:59 +0200

    Remove temporary code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,14 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "theme";
 import FrontPage from "./scenes/front-page/FrontPage";
-import DestinationReview from "components/DestinationReview/DestinationReview";
 import { BrowserRouter } from "react-router-dom";
-import { useState } from "react";
 
 function App() {
-  const [reviewingDestinationId, setReviewingDestinationId] = useState("1");
-
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {reviewingDestinationId ? (
-          <DestinationReview
-            destinationName="Ounasvaara"
-            imageSrc="https://www.visitrovaniemi.fi/wp-content/uploads/Ounasvaara-Ski-Centre-2-900x505.jpg"
-            city="Rovaniemi"
-            country="Finland"
-          />
-        ) : (
-          <FrontPage />
-        )}
+        <FrontPage />
       </ThemeProvider>
     </BrowserRouter>
   );
