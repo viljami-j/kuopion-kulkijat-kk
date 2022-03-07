7e5b0508336d775863e0598bd0d4793376a9519b
ArttuOll <arttuoll@tutanota.com>
2022-03-07 19:28:09 +0200

    Install cors middleware for Express
    This helps us setup CORS easily. Without this all requests to backend fail
    due to missing Access-Control-Allow-Origin headers

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const apiRouter = require('./routes');
+const express = require("express");
+const apiRouter = require("./routes");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use("/destinations", apiRouter);
 
-app.use('/destinations', apiRouter);
-
-app.listen(process.env.PORT || '3000', () =>{
-    console.log("port 3000");
+app.listen(process.env.PORT || "3000", () => {
+  console.log("Port 3000");
 });
