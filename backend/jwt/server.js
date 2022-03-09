99694d14aa35cd1587e6ad30eabaaa8e891ee77f
Artturi Toivainen <Artturi.toivainen@gmail.com>
2022-03-09 18:20:49 +0200

    jwt setti

---
+++ backend/jwt/server.js
@@ -0,0 +1,45 @@
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username:'Kyle',
+        title:'Post1'
+    },
+    {
+        username:'mikko',
+        title:'Post2'
+    }
+]
+
+app.get('/posts', authenticateToken,(req,res) =>{
+    
+res.json(posts.filter(post=> post.username === req.user.name));
+})
+
+app.post('/login', (req,res)=>{
+const username = req.body.username
+const user = { name: username}
+const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+res.json({accessToken: accessToken})
+})
+
+function authenticateToken(req,res,next){
+const autHeader = req.headers['authorization']
+const token =autHeader && autHeader.split(' ')[1]
+if(token == null) return res.sendStatus(401)
+
+jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+    if(err) return res.sendStatus(403)
+    req.user = user
+    next()
+})
+}
+
+app.listen(3000)
