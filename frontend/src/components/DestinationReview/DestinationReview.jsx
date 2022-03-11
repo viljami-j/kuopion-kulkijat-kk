bbdc5a2c350f1feffb37cddd5222a2b8d3a10919
ArttuOll <arttuoll@tutanota.com>
2022-03-11 17:28:05 +0200

    Refactor DestinationReview

--- frontend/src/components/DestinationReview/DestinationReview.jsx
+++ frontend/src/components/DestinationReview/DestinationReview.jsx
@@ -1,25 +1,9 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import PropTypes from "prop-types";
 import Navbar from "../Navbar/Navbar";
 import DestinationBackgroundImage from "../PictureWrapWithGradientCover/DestinationBackgroundImage";
 import LocationIndicator from "../DestinationCard/LocationIndicator";
-
-export function ApplyRowBreaks({ count }) {
-  return [...Array(count)].map((e, i) => (
-    <Grid
-      item
-      xs={12}
-      sx={{
-        userSelect: "none",
-        msUserSelect: "none",
-        MozUserSelect: "none",
-        WebkitUserSelect: "none",
-      }}
-    >
-      ⠀
-    </Grid>
-  ));
-}
+import { Box } from "@mui/system";
 
 DestinationReview.defaultProps = {
   imageSrc: "",
@@ -46,51 +30,41 @@ export default function DestinationReview({
   return (
     <Container>
       <Navbar />
-      <Grid container spacing={2} rowSpacing={4} sx={{ mt: "-10px" }}>
-        <Grid item xs={12}>
-          <DestinationBackgroundImage
-            imageSrc={imageSrc}
-            gradientType={"bottomToTop"}
+      <DestinationBackgroundImage imageSrc={imageSrc} direction={"bottomToTop"}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "end",
+            height: "100%",
+            width: "100%",
+            pb: 3,
+            pl: 3,
+          }}
+          component="header"
+        >
+          <Typography
+            variant="h1"
+            sx={{ color: "white" }}
+            fontSize={"48px"}
+            fontWeight={"medium"}
           >
-            <Grid container>
-              <ApplyRowBreaks count={18} />
-              <Grid item xs={0.3} />
-              <Grid item xs={8}>
-                <Typography
-                  variant="h1"
-                  sx={{ color: "white" }}
-                  fontSize={"48px"}
-                  fontWeight={"medium"}
-                >
-                  {destinationName}
-                </Typography>
-              </Grid>
-              <Grid item xs={3.7} />
-              <Grid item xs={0.3} />
-              <Grid item xs={8}>
-                {city && country ? (
-                  <div
-                    style={{
-                      color: "white",
-                      maxWidth: "80%",
-                      textAlign: "start",
-                    }}
-                  >
-                    <LocationIndicator
-                      location={{ city, country }}
-                      fontsize="16px"
-                    />
-                  </div>
-                ) : null}
-              </Grid>
-            </Grid>
-          </DestinationBackgroundImage>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography sx={{ fontSize: "14px" }}>{description}</Typography>
-        </Grid>
-        <ApplyRowBreaks count={1} />
-      </Grid>
+            {destinationName}
+          </Typography>
+          {city && country ? (
+            <Box
+              style={{
+                color: "white",
+                maxWidth: "80%",
+                textAlign: "start",
+              }}
+            >
+              <LocationIndicator location={{ city, country }} fontsize="16px" />
+            </Box>
+          ) : null}
+        </Box>
+      </DestinationBackgroundImage>
+      <Typography sx={{ lineHeight: 1.6, mt: 3 }}>{description}</Typography>
     </Container>
   );
 }
