20115dbe8f1087e65ad6c6cea20a9fcd0ac7156f
ArttuOll <arttuoll@tutanota.com>
2022-03-11 15:05:03 +0200

    Add support for aborting network requests

--- frontend/src/util/makeApiRequest.js
+++ frontend/src/util/makeApiRequest.js
@@ -2,10 +2,11 @@ import { isEmpty } from "lodash";
 
 function makeApiRequest(method) {
   return function withPath(path) {
-    return async function withBody(body) {
+    return async function withBody(body, abortController) {
       const apiUrl = `${process.env.REACT_APP_BACKEND_URL}${path}`;
       const response = await fetch(apiUrl, {
-        method: method,
+        method,
+        signal: abortController.signal,
         headers: {
           "Content-Type": "application/json;charset=utf-8",
         },
