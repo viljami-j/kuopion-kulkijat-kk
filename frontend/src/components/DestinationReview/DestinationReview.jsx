6b16f547f60d7af23c8bb74dc1caaad52b1270dd
ArttuOll <arttuoll@tutanota.com>
2022-03-11 17:02:24 +0200

    Refactor PictureWrapWithGradientCover

--- frontend/src/components/DestinationReview/DestinationReview.jsx
+++ frontend/src/components/DestinationReview/DestinationReview.jsx
@@ -1,8 +1,8 @@
-import { Grid, Typography, Container } from "@mui/material";
-import LocationIndicator from "components/DestinationCard/LocationIndicator";
-import PictureWrapWithGradientCover from "components/PictureWrapWithGradientCover/PictureWrapWithGradientCover";
+import { Container, Grid, Typography } from "@mui/material";
 import PropTypes from "prop-types";
 import Navbar from "../Navbar/Navbar";
+import DestinationBackgroundImage from "../PictureWrapWithGradientCover/DestinationBackgroundImage";
+import LocationIndicator from "../DestinationCard/LocationIndicator";
 
 export function ApplyRowBreaks({ count }) {
   return [...Array(count)].map((e, i) => (
@@ -48,44 +48,43 @@ export default function DestinationReview({
       <Navbar />
       <Grid container spacing={2} rowSpacing={4} sx={{ mt: "-10px" }}>
         <Grid item xs={12}>
-          <PictureWrapWithGradientCover
+          <DestinationBackgroundImage
             imageSrc={imageSrc}
-            gradientType={"rising"}
-            wrappedElement={
-              <Grid container>
-                <ApplyRowBreaks count={18} />
-                <Grid item xs={0.3} />
-                <Grid item xs={8}>
-                  <Typography
-                    variant="h1"
-                    sx={{ color: "white" }}
-                    fontSize={"48px"}
-                    fontWeight={"medium"}
-                  >
-                    {destinationName}
-                  </Typography>
-                </Grid>
-                <Grid item xs={3.7} />
-                <Grid item xs={0.3} />
-                <Grid item xs={8}>
-                  {city && country ? (
-                    <div
-                      style={{
-                        color: "white",
-                        maxWidth: "80%",
-                        textAlign: "start",
-                      }}
-                    >
-                      <LocationIndicator
-                        location={{ city, country }}
-                        fontsize="16px"
-                      />
-                    </div>
-                  ) : null}
-                </Grid>
+            gradientType={"bottomToTop"}
+          >
+            <Grid container>
+              <ApplyRowBreaks count={18} />
+              <Grid item xs={0.3} />
+              <Grid item xs={8}>
+                <Typography
+                  variant="h1"
+                  sx={{ color: "white" }}
+                  fontSize={"48px"}
+                  fontWeight={"medium"}
+                >
+                  {destinationName}
+                </Typography>
               </Grid>
-            }
-          />
+              <Grid item xs={3.7} />
+              <Grid item xs={0.3} />
+              <Grid item xs={8}>
+                {city && country ? (
+                  <div
+                    style={{
+                      color: "white",
+                      maxWidth: "80%",
+                      textAlign: "start",
+                    }}
+                  >
+                    <LocationIndicator
+                      location={{ city, country }}
+                      fontsize="16px"
+                    />
+                  </div>
+                ) : null}
+              </Grid>
+            </Grid>
+          </DestinationBackgroundImage>
         </Grid>
         <Grid item xs={12}>
           <Typography sx={{ fontSize: "14px" }}>{description}</Typography>
--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,36 +1,35 @@
 import { Grid, Typography } from "@mui/material";
 import PropTypes from "prop-types";
-import PictureWrapWithGradientCover from "components/PictureWrapWithGradientCover/PictureWrapWithGradientCover";
+import DestinationBackgroundImage from "components/PictureWrapWithGradientCover/DestinationBackgroundImage";
 
 export default function Hero({ imageSrc, header, caption }) {
   return (
     <Grid item xs={12} align="center">
-      <PictureWrapWithGradientCover
+      <DestinationBackgroundImage
         imageSrc={imageSrc}
-        gradientType={"horizontal"}
-        wrappedElement={
-          <div style={{ marginTop: "2%" }}>
-            <Typography
-              variant="h1"
-              style={{ color: "white" }}
-              sx={{ pt: 4, pb: 2 }}
-              fontSize={"48px"}
-              fontWeight={"medium"}
-            >
-              {header}
-            </Typography>
-            <Typography
-              style={{
-                color: "white",
-                maxWidth: "80%",
-                textAlign: "start",
-              }}
-            >
-              {caption}
-            </Typography>
-          </div>
-        }
-      />
+        gradientType={"topToBottom"}
+      >
+        <div style={{ marginTop: "2%" }}>
+          <Typography
+            variant="h1"
+            style={{ color: "white" }}
+            sx={{ pt: 4, pb: 2 }}
+            fontSize={"48px"}
+            fontWeight={"medium"}
+          >
+            {header}
+          </Typography>
+          <Typography
+            style={{
+              color: "white",
+              maxWidth: "80%",
+              textAlign: "start",
+            }}
+          >
+            {caption}
+          </Typography>
+        </div>
+      </DestinationBackgroundImage>
     </Grid>
   );
 }
