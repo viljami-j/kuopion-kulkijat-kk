3469771cb7c26d0e10151e58a6f4768a51d745b7
ArttuOll <arttuoll@tutanota.com>
2022-03-15 19:30:02 +0200

    Decrease destination card header font size

--- frontend/src/components/DestinationCard/DestinationCard.jsx
+++ frontend/src/components/DestinationCard/DestinationCard.jsx
@@ -59,7 +59,7 @@ function DestinationCard({ id, name, description, image, city, country }) {
           titleTypographyProps={{
             component: "h3",
             fontWeight: "medium",
-            fontSize: "max(1rem, 1.25vw)",
+            fontSize: "max(1rem, 1.15vw)",
             textAlign: "start",
           }}
           sx={{ pb: 0 }}
