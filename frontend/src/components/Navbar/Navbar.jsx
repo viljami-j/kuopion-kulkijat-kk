431e674d7b00aa51ac3cfee9f5524966db343a95
ArttuOll <arttuoll@tutanota.com>
2022-03-24 20:00:25 +0200

    Add back navigation button to nav bar when viewing destinations

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
-import { AppBar, Slide, Toolbar, useScrollTrigger } from "@mui/material";
+import {
+  AppBar,
+  IconButton,
+  Slide,
+  Toolbar,
+  useScrollTrigger,
+} from "@mui/material";
 import NavbarButtons from "./NavbarButtons";
 import PropTypes from "prop-types";
 import StyledLink from "./styled/StyledLink";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { ArrowBack } from "@mui/icons-material";
 
 Navbar.defaultProps = {
   loggedInName: "",
@@ -14,12 +21,26 @@ Navbar.propTypes = {
 
 export default function Navbar({ loggedInName }) {
   const trigger = useScrollTrigger();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const shouldEnableBackNavigation = new RegExp(/\/destinations\/\d/).test(
+    location.pathname
+  );
 
   return (
     <>
       <Slide appear={false} direction={"down"} in={!trigger}>
         <AppBar position="fixed" sx={{ backgroundColor: "white" }}>
           <Toolbar>
+            {shouldEnableBackNavigation ? (
+              <IconButton
+                aria-label="Navigoi takaisin"
+                onClick={() => navigate(-1)}
+              >
+                <ArrowBack sx={{ color: "black", mr: 1 }} />
+              </IconButton>
+            ) : null}
             <StyledLink to="/" component={Link}>
               Kuopion Kulkijat
             </StyledLink>
