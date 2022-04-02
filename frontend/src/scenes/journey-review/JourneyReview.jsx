395f2b713f47d57d4d78661717f264f7c0061e0f
ArttuOll <arttuoll@tutanota.com>
2022-04-02 16:21:17 +0300

    Create JourneyReview component

--- frontend/src/scenes/journey-review/JourneyReview.jsx
+++ frontend/src/scenes/journey-review/JourneyReview.jsx
@@ -1,7 +1,45 @@
-// import testData from "test-data/testJourneys.json";
+import { CircularProgress, Typography } from "@mui/material";
+import React from "react";
+import { useParams } from "react-router-dom";
+import StoryCard from "../../components/JourneyCard/StoryCard";
+import useJourneys from "../../util/hooks/useJourneys";
+import { Box } from "@mui/system";
 
 function JourneyReview() {
-  return <></>;
+  const { id } = useParams();
+  const { journeys, isLoadingJourneys, JourneyLoadingSnackbar } =
+    useJourneys(id);
+  const journey = journeys[0];
+
+  const startDate = new Date(journey.startDate).toLocaleDateString();
+
+  const endDate = new Date(journey.endDate).toLocaleDateString();
+
+  return (
+    <>
+      <Typography
+        variant="h1"
+        sx={{ my: 4 }}
+      >{`Matkakertomus ${startDate} - ${endDate}`}</Typography>
+      {isLoadingJourneys ? (
+        <CircularProgress sx={{ mx: "auto", my: 20 }} />
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            gap: 2,
+          }}
+        >
+          {journey.stories.map((story) => (
+            <StoryCard {...story} key={story.date} />
+          ))}
+        </Box>
+      )}
+      <JourneyLoadingSnackbar />
+    </>
+  );
 }
 
 export default JourneyReview;
