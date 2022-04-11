04ddc59cc1739daec2db3ecc498c0f4ff07aac53
Moo-hi <viljami.j.m@gmail.com>
2022-04-12 01:09:26 +0300

    Fix too small maximum payload size

--- backend/api/app.js
+++ backend/api/app.js
@@ -8,9 +8,8 @@ app.use(cors(corsOptions));
 var corsOptions = {
   origin: "http://localhost:3000",
 };
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 require("./routes/routes.js")(app);
 app.set("views", path.join(__dirname, "views"));
@@ -27,7 +26,7 @@ app.use(
   })
 );
 
-app.use('/', pageRouter)
+app.use("/", pageRouter);
 
 app.use((req, res, next) => {
   var err = new Error("Page not found");
