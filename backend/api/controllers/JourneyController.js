579e138be81d188ae02a117663207d03ac27ec4f
Viljami Mäkelä <viljami.j.m@gmail.com>
2022-04-25 07:29:03 +0300

    Add to journeys-endpoint: GET journey by journey id @ /api/journeys/:idmatka + fix critical bug in fetching story images in all journey-related endpoints + tiny refactoring

--- backend/api/controllers/JourneyController.js
+++ backend/api/controllers/JourneyController.js
@@ -1,5 +1,23 @@
 const Journey = require("../models/journey.model.js");
 
+exports.findJourneysByJourneyId = (req, res) => {
+  Journey.findByJourneyId(req.params.idmatka, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Didn't find any journeys with id ${req.params.idmatka}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving a journey with id " + req.params.idmatka,
+        });
+      }
+    } else {
+      res.send(data);
+    }
+  });
+};
+
 exports.findJourneysByUserId = (req, res) => {
   Journey.findByUserId(req.params.idmatkaaja, (err, data) => {
     if (err) {
